6d4ccd7e1cdcf2d7050a6f61e0451c0cb6173f18
pldigitalg-ui <pldigitalg@gmail.com>
2026-02-12 17:19:24 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,11 @@
+// js/config.js
+window.APP_CONFIG = {
+  storeName: "LP Grill",
+  whatsapp: "5531998064556", // <-- TROQUE AQUI (DDI+DDD+NUMERO, só números)
+  currency: "BRL",
+  limits: {
+    fruitsFree: 3,
+    extrasIncluded: 2
+  }
+};
 
