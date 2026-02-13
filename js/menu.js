ddc7c1e927dd57806d7cbddd6864ccfe865bd878
pldigitalg-ui <pldigitalg@gmail.com>
2026-02-13 10:24:43 -0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,19 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(){
 
   const botao = document.getElementById("menuToggle");
-  const menu = document.getElementById("siteMenu");
   const overlay = document.getElementById("menuOverlay");
 
-  if(!botao || !menu || !overlay) return;
+  if(!botao || !overlay) return;
 
   botao.addEventListener("click", function(){
-    menu.classList.toggle("ativo");
-    overlay.classList.toggle("ativo");
+    document.body.classList.toggle("corpo-menu-abrir");
   });
 
   overlay.addEventListener("click", function(){
-    menu.classList.remove("ativo");
-    overlay.classList.remove("ativo");
+    document.body.classList.remove("corpo-menu-abrir");
   });
 
 });
