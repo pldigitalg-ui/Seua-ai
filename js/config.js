ad1040ef1a4278d2ce375d4d2abfbe52514b474a
pldigitalg-ui <pldigitalg@gmail.com>
2026-02-13 15:32:29 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // js/app-config.js
 window.APP_CONFIG = {
   brand: "LP Grill Açaí",
-  whatsapp: "5531998064556", // só números
+  whatsapp: "553198832407", // só números
   deliveryFee: 0 // taxa fixa opcional (0 = sem taxa)
 };
