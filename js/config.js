4d24c171af9164fa7cb1652a635bd6ddc956a224
pldigitalg-ui <pldigitalg@gmail.com>
2026-02-13 15:42:11 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,5 @@
-// js/app-config.js
-janela.CONFIGURACAO_DO_APLICATIVO = {
-  marca:"LP Grill Açaí",
-  whatsapp:"553198832407",
-  Taxa_de_entrega:0 // taxa fixa opcional (0 = sem taxa)
-}
+window.CONFIGURACAO_DO_APLICATIVO = {
+  marca: "LP Grill Açaí",
+  whatsapp: "553198832407",
+  taxa_de_entrega: 0
+};
